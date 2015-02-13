a3c81b8dd10d20ef31d72c028faaf7b0640a039e
mtbtiago <tiago.ameller@gmail.com>
2015-02-13 17:46:54 +0100

    refactoring to pass object info

--- src/javascript/application.js
+++ src/javascript/application.js
@@ -15,15 +15,17 @@ document.querySelector("#search_form").addEventListener("submit", function(event
   while (song_list.firstChild) {
     song_list.removeChild(song_list.firstChild);
   }
+  // display tab list
+  document.querySelector("#search_tab").click();
 
   ajax(url, {}, function(response) {
     try {
       console.log(response);
       if (response.tracks.items.length === 0) {
-        createSong(song_list, "No songs found!","");
+        createSong(song_list, "No songs found!", "");
       } else {
         response.tracks.items.forEach(function(item) {
-          createSong(song_list, item.album.name, item.artists[0].name,item.id);
+          createSong(song_list, item.album.name, item.artists[0].name, item.id);
         });
       }
     } catch (e) {
@@ -37,8 +39,10 @@ function createSong(parent, title, author, id) {
   var li = document.createElement("li");
   var h3 = document.createElement("h3");
   h3.innerHTML = title;
-  h3.id = id;
-  h3.addEventListener("click",songClick);
+  if (id) {
+    h3.id = id;
+  }
+  h3.addEventListener("click", songClick);
   var h4 = document.createElement("h4");
   h4.innerHTML = author;
   parent.appendChild(li);
@@ -58,6 +62,10 @@ function ajax(url, data, callback) {
 };
 
 function songClick() {
-  // console.log(this.id);
-  
+  if (this.id) {
+    document.querySelector("#playing_tab").click();
+    document.querySelector("#album_img").src="https://i.scdn.co/image/c6aa825443428782713e047893aa043a30cd357d";
+
+    console.log(this.id);
+  }
 }
