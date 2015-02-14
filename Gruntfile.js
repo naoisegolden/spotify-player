a63305b756740652ae947ad6fc304de80bb7ed35
Naoise Golden <naoise.golden@xing.com>
2015-02-14 18:26:30 +0100

    Complete html and styles with grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,9 +9,43 @@ module.exports = function (grunt) {
 
     // Define the configuration for all the tasks
     grunt.initConfig({
-        // Project settings
-        config: config
+      // Project settings
+      config: config,
+
+      sass: {
+        dist: {
+          files: {
+            '<%= config.app %>/styles/style.css': '<%= config.app %>/styles/style.scss'
+          }
+        }
+      },
+
+      postcss: {
+        options: {
+          map: true,
+          processors: [
+            require('autoprefixer-core')({ browsers: 'last 2 versions' }).postcss
+          ]
+        },
+        dist: {
+          src: '<%= config.app %>/styles/style.css'
+        }
+      },
+
+      watch: {
+        files: ['Gruntfile.js', '<%= config.app %>/**/*.scss', '<%= config.app %>/**/*.html'],
+        tasks: ['default'],
+        options: {
+          livereload: true,
+        }
+      }
     });
 
-    grunt.registerTask('default', []);
+    // Load Tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-postcss');
+
+    // Custom Tasks
+    grunt.registerTask('default', ['sass', 'postcss']);
 };
