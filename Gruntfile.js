4939fe1a629596a9138d9ffc583e975c55bbafce
Naoise Golden <naoise.golden@xing.com>
2015-02-15 20:18:16 +0100

    Build and deploy tasks, finished production version

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,20 +32,87 @@ module.exports = function (grunt) {
         }
       },
 
+      cssmin: {
+        options: {
+          rebase: false
+        },
+        dist: {
+          files: {
+            '<%= config.dist %>/styles/style.min.css': '<%= config.app %>/styles/style.css'
+          }
+        }
+      },
+
+      uglify: {
+        options: {
+          mangle: false
+        },
+        dist: {
+          files: {
+            '<%= config.dist %>/javascript/application.min.js': ['<%= config.app %>/javascript/application.js']
+          }
+        }
+      },
+
+      imagemin: {
+        dist: {
+          files: [
+            {
+              expand: true,
+              cwd: '<%= config.app %>',
+              src: ['images/*.{png,jpg,gif}'],
+              dest: '<%= config.dist %>'
+            }
+          ]
+        }
+      },
+
+      copy: {
+        main: {
+          expand: true,
+          cwd: '<%= config.app %>',
+          src: 'favicon.ico',
+          dest: '<%= config.dist %>/',
+        }
+      },
+
+      processhtml: {
+        dist: {
+          files: {
+            '<%= config.dist %>/index.html': '<%= config.app %>/index.html'
+          }
+        }
+      },
+
       watch: {
         files: ['Gruntfile.js', '<%= config.app %>/**/*.scss', '<%= config.app %>/**/*.html'],
         tasks: ['default'],
         options: {
           livereload: true,
         }
+      },
+
+      'gh-pages': {
+        options: {
+          base: '<%= config.dist %>'
+        },
+        src: ['**']
       }
     });
 
     // Load Tasks
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-gh-pages');
 
     // Custom Tasks
     grunt.registerTask('default', ['sass', 'postcss']);
+    grunt.registerTask('build', ['default', 'uglify', 'cssmin', 'imagemin', 'copy', 'processhtml']);
+    grunt.registerTask('deploy', ['build', 'gh-pages']);
 };
--- readme.md
+++ readme.md
@@ -14,7 +14,11 @@ Download dependencies:
 
 Build production version and deploy to your Github Page:
 
-    $ grunt
     $ grunt deploy
 
 The app will be available at https://YOUR_USERNAME.github.io/spotify-player
+
+Other available grunt tasks are:
+
+    $ grunt build # creates production version in `build` folder
+    $ grunt watch # generates css when files change and reloads browser with livereload
